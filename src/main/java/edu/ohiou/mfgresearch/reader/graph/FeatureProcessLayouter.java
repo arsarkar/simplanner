e3ac41e0dd8bf121d94c8686456b0cb8593e1874
sormaz <sormaz@gmail.com>
2019-05-22 10:30:35 -0400

    added FeatureProcessLayouter

---
+++ src/main/java/edu/ohiou/mfgresearch/reader/graph/FeatureProcessLayouter.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package edu.ohiou.mfgresearch.reader.graph;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
+import edu.ohiou.mfgresearch.labimp.graph.*;
+
+/**
+ * @author sormaz
+ *
+ */
+public class FeatureProcessLayouter extends GraphLayouter {
+	
+	double currentX = 0.0;
+	double currentY = 0.0;
+
+	/**
+	 * @param graph
+	 * @param point
+	 */
+	public FeatureProcessLayouter(Graph graph, Double point) {
+		super(graph, point);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void nodeAdded(Node n) {
+		vertices.put(n, new Vertex (n, new Point2D.Double(currentX, currentY)));
+		currentX += 1.0;
+		currentY += 1.0;
+		if (canvas != null) canvas.repaint();
+	}
+
+}
